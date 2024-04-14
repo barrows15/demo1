e00e9d9dadb18e89f6045982c24e5087eddaeff2
nayanashutosh@gmail.com <corncoder@12>
2024-04-14 19:44:37 +0530

    first commit

---
+++ html1.js
@@ -0,0 +1,15 @@
+
+function bigImg(x) {
+  x.style.height = "200px";
+  x.style.width = "200px";
+}
+
+function normalImg(x) {
+  x.style.height = "100px";
+  x.style.width = "100px";
+}
+
+function onsubmit1(){
+alert('onclick');
+alert('Thank you');
+}
